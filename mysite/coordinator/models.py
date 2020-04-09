187c5c2d7f88431fa61dc28cc915d552f3b7fcac
Grenadeer <svertilov.alexey@gmail.com>
2020-04-09 22:28:33 +0300

    Record model added.

--- mysite/coordinator/models.py
+++ mysite/coordinator/models.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.db import models
-from django.utils import timezone
+#from django.utils import timezone
 
 
 class Doctor(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
+        null=True,
+        blank=True,
         help_text="Укажите учетную запись пользователя",
         verbose_name="Учетная запись",
     )
@@ -23,6 +25,48 @@ class Doctor(models.Model):
         verbose_name = "Врач"
         verbose_name_plural = "Врачи"
 
-    #def records(self):
-    #    records = Record.objects.filter(doctor=self)
-    #    return records
+    def records(self):
+        records = Record.objects.filter(doctor=self)
+        return records
+
+
+class Record(models.Model):
+    date = models.DateField(
+        auto_now_add=True,
+        help_text="Дата регистрации вызова",
+        verbose_name="Дата вызова",
+    )
+    doctor = models.ForeignKey(
+        Doctor,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='doctor_records',
+        help_text="Врач назначенный на вызов",
+        verbose_name="Врач",
+    )
+    address = models.CharField(
+        max_length=200,
+        help_text="Адрес вызова",
+        verbose_name="Адрес",
+    )
+    record_order = models.IntegerField(
+        help_text="Порядковый номер вызова",
+        verbose_name="Порядок",
+    )
+
+    def __str__(self):
+        temp = "Вызов от " + self.date.strftime('%d.%m.%Y') + " по адресу <" + self.address + ">"
+        if self.doctor is None:
+            temp = temp + " не назначен"
+        else:
+            temp = temp + " назначен " + str(self.doctor)
+        return temp
+
+    class Meta:
+        verbose_name = "Вызов"
+        verbose_name_plural = "Вызовы"
+
+    @staticmethod
+    def unrelated():
+        return Record.objects.filter(doctor=None)
