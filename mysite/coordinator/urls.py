db3b3ce0a47e5311ac0817c3231824924134c5ea
grenadeer <svertilov.alexey@gmail.com>
2020-04-10 13:29:27 +0300

    Simple doctor list complete.

--- mysite/coordinator/urls.py
+++ mysite/coordinator/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.views.generic import ListView
 from . import views
+from .models import Doctor
 
 urlpatterns = [
     path('', views.record_summary, name='record_summary'),
     path('record/summary', views.record_summary, name='record_summary'),
+
+    path('doctor/', ListView.as_view(queryset=Doctor.objects.all(), template_name='coordinator/doctor_list.html'), name='doctor_list'),
 ]
