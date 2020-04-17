eac0804022e439f9c611ff5cead9b1bfa86fbcac
grenadeer <ipsv@narod.ru>
2020-04-17 20:04:34 +0300

    Department model registered in admin interface. Initial data added to migrations for ServiceType and Department.

--- mysite/coordinator/admin.py
+++ mysite/coordinator/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Doctor, Record, ServiceType
+from .models import Department, Doctor, Record, ServiceType
 
 
+admin.site.register(Department)
 admin.site.register(Doctor)
 admin.site.register(Record)
 admin.site.register(ServiceType)
